032e1a15498e08732b3e011131c4016362e5862b
Kevin Parker <kevin.parker.727@gmail.com>
2024-07-07 15:35:20 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,1 @@
-/** @type {import('next').NextConfig} */
-import dotenv from "dotenv";
-import { join } from "path";
 
-dotenv.config({ path: join(process.cwd(), "env.local") });
-
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    GOOGLE_ID: process.env.GOOGLE_ID,
-    GOOGLE_SECRET: process.env.GOOGLE_SECRET,
-  },
-};
-
-export default nextConfig;
