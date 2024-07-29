ab4d63c5d913c4a7a9b55524b4afeddc984ba75d
kevinparker727 <kevin.parker.727@gmail.com>
2024-07-29 14:30:12 -0400

    Sign in menu item file rearranging

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,10 @@
 import { withAuth } from "next-auth/middleware";
 
-export default withAuth();
-
-export const config = {
-  matcher: ["/protected/:path*", "/api/protected/:path*"],
-};
+export default withAuth({
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout",
+    error: "/auth/error", // Error code passed in query string as ?error=
+    verifyRequest: "/auth/verify-request", // (used for check email message)
+  },
+});
--- src/pages/auth/signin/SignIn.tsx
+++ src/pages/auth/signin/SignIn.tsx
@@ -1,24 +1,11 @@
 import Login from "@/components/Login";
 import Box from "@mui/material/Box";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import { useSession } from "next-auth/react";
-import NextLink from "next/link";
-import React from "react";
-import scss from "./SignIn.module.scss";
-import { signOut } from "next-auth/react";
 
-const menuRouteList = ["", "data", "profile", "settings"];
-const menuListTranslations = ["Home", "Data", "Profile", "Settings"];
+import { useSession } from "next-auth/react";
+import React from "react";
 
 const SignIn = () => {
   const { data: session } = useSession();
-
-  const handleListItemButtonClick = (text: string) => {
-    text === "Sign Out" ? signOut() : null;
-  };
 
   return (
     <Box
@@ -34,31 +21,6 @@ const SignIn = () => {
         {session ? (
           <div>
             <div>Thank you signing in.</div>
-            <div>
-              <List>
-                {menuListTranslations.map((text, index) => (
-                  <ListItem key={text} disablePadding sx={{ display: "block" }}>
-                    <NextLink
-                      className={scss.link}
-                      href={`/dashboard/${menuRouteList[index]}`}
-                      passHref
-                    >
-                      <ListItemButton
-                        onClick={() => handleListItemButtonClick(text)}
-                        title={text}
-                        aria-label={text}
-                        sx={{
-                          minHeight: 48,
-                          px: 2.5,
-                        }}
-                      >
-                        <ListItemText primary={text} />
-                      </ListItemButton>
-                    </NextLink>
-                  </ListItem>
-                ))}
-              </List>
-            </div>
           </div>
         ) : (
           "Please log in."
